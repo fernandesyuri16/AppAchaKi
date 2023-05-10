1a11d9dd92a225a89474ff97a4030545ab8129d1
Guilherme Nicula <guinicula@gmail.com>
2023-05-09 22:11:45 -0300

    Criação da Tela de busca

--- InicioScreen.js
+++ InicioScreen.js
@@ -1,115 +1,30 @@
-import {
-	Animated,
-	Keyboard,
-	KeyboardAvoidingView,
-	Platform,
-	StyleSheet,
-	Text,
-	TextInput,
-	TouchableOpacity,
-	View,
-	ImageBackground,
-} from 'react-native'
+import {Image, Animated, Platform, StyleSheet, Text, TextInput, TouchableOpacity, View, ImageBackground,} from 'react-native'
 import React, {Component} from 'react'
-
-const initialState = {
-	offset: new Animated.ValueXY({x: 0, y: 95}),
-	opacity: new Animated.Value(0),
-	logo: new Animated.ValueXY({x: 1, y: 1}),
-}
-
-
-
 export default class InicioScreen extends Component {
-
-	state = {...initialState}
-
-	keyboardDidShow = () => {
-		Animated.parallel([
-			Animated.timing(this.state.logo.x, {
-				toValue: 0.7,
-				duration: 35,
-				useNativeDriver: true,
-			}).start(),
-			Animated.timing(this.state.logo.y, {
-				toValue: 0.7,
-				duration: 35,
-				useNativeDriver: true,
-			}).start(),
-		])
-	}
-	keyboardDidHide = () => {
-		Animated.parallel([
-			Animated.timing(this.state.logo.x, {
-				toValue: 1,
-				duration: 35,
-				useNativeDriver: true,
-			}).start(),
-			Animated.timing(this.state.logo.y, {
-				toValue: 1,
-				duration: 35,
-				useNativeDriver: true,
-			}).start(),
-		])
-	}
-
-	componentDidMount = () => {
-		Keyboard.addListener('keyboardDidShow', this.keyboardDidShow)
-		Keyboard.addListener('keyboardDidHide', this.keyboardDidHide)
-		Animated.parallel([
-			Animated.spring(this.state.offset, {
-				toValue: 0,
-				speed: 4,
-				bounciness: 10,
-				useNativeDriver: true,
-			}).start(),
-			Animated.timing(this.state.opacity, {
-				toValue: 1,
-				duration: 800,
-				bounciness: 10,
-				useNativeDriver: true,
-			}).start(),
-		])
-	}
 
 	render() {
 		return (
-			<KeyboardAvoidingView
+			<View
 				style={styles.background}
 				behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
 				<ImageBackground source={require('./assets/Fundo.jpg')}
     			style={styles.backgroundImage}>
-					<View style={styles.logocontainer}>
-						<Animated.Image
-							style={ {
-								height: 150,
-								width: 160,
-								transform: [
-									{scaleX: this.state.logo.x},
-									{scaleY: this.state.logo.y},
-								],
-							}}
-							source={require('./assets/logoGG.png')}
+					<View style={styles.topo}>
+						<Image style={styles.logo} source={require('./assets/favicon.png')}/>
+						<TextInput
+							placeholderTextColor="#3337"
+							style={styles.input}
+							placeholder="Buscar..."
+							autoCorrect={false}
+							onChangeText={() => {}}
+
 						/>
+						<View style={styles.meio}>
+							<Text style={styles.text}>Serviços mais buscados</Text>
+						</View>
 					</View>
-					<Animated.View
-						style={[
-							styles.container,
-							{
-								opacity: this.state.opacity,
-								transform: [
-									{
-										translateY: this.state.offset.y,
-									},
-								],
-							},
-						]}>
-						<TouchableOpacity style={styles.submitbutton}>
-							<Text onPress={() => this.props.navigation.navigate ("HomeScreen")} style={styles.submittext}>Teste - Voltar</Text>
-						</TouchableOpacity>
-					</Animated.View>
 				</ImageBackground>
-			</KeyboardAvoidingView>
+			</View>
 		)
 	}
 }
@@ -122,10 +37,24 @@ const styles = StyleSheet.create({
 		backgroundColor: '#333',
 		backgroundImage: 'url(${backgroundImg})',
 	},
-	logocontainer: {
-		flex: 1,
+	topo:{
+		flexDirection: 'column',
+		backgroundColor: '#282828',
+		alignItems: 'center'
+	},
+	meio: {
+		backgroundColor: 'grey',
+		width: '100%',
+		marginTop: '2%',
+	},
+	logo: {
 		alignItems: 'center',
 		justifyContent: 'center',
+		resizeMode: 'cover',
+		marginTop: '8%',
+		height: '35%',
+		width: '35%'
+
 	},
 	container: {
 		flex: 1,
@@ -136,44 +65,18 @@ const styles = StyleSheet.create({
 	input: {
 		backgroundColor: '#FFF',
 		width: '90%',
-		marginBottom: 20,
 		color: '#333',
 		fontSize: 17,
-		padding: 10,
-	},
-	submitbutton: {
-		backgroundColor: '#282828',
-		borderWidth: 2,
-		borderColor: 'white',
-		width: '50%',
-		height: 45,
-		justifyContent: 'center',
-		alignItems: 'center',
-		borderRadius: 40,
-	},
-	submittext: {
-		marginTop: 5,
-		color: '#FFF',
-		fontSize: 18,
-	},
-	registerText: {
-		fontSize: 15,
-		color: '#FFF',
-		elevation: 100,
-		marginTop: 15,
-	},
-	registerText2: {
-		fontSize: 17,
-		color: '#282828',
-		fontWeight: '900',
-		textShadowColor: 'white',
-		textShadowOffset: { width: 0, height: 1, },
-		textShadowRadius: 1,
+		padding: 5,
 	},
  	text: {
-		fontSize: 20,
+		fontSize: 24,
+		fontWeight: 900,
 		color: '#FFF',
 		marginBottom: 30,
+		marginTop: '5%',
+		textAlign: 'center',
+		textAlignVertical: 'top'
   	},
 	backgroundImage: {
 		flex: 1,
